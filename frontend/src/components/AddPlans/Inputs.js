bf4164752265552ca674d2c2e9c673036c7df36f
Jun0zo <joon0zo1022@gmail.com>
2022-06-25 12:44:42 +0900

    직원추가 페이지 뼈대 잡기

---
+++ frontend/src/components/AddPlans/Inputs.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
+function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+function ResponsiveDatePickers() {
+  const [value, setValue] = React.useState(new Date());
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        disableFuture
+        label="Responsive"
+        openTo="year"
+        views={['year', 'month', 'day']}
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default function Inputs() {
+  return (
+    <div>
+      <Stack spacing={3}>
+        <BasicSelect />
+        <BasicSelect />
+        <ResponsiveDatePickers />
+      </Stack>
+    </div>
+  );
+}
--- frontend/src/pages/AddPlans.js
+++ frontend/src/pages/AddPlans.js
@@ -2,26 +2,31 @@ import React from 'react';
 import TimePicker from '../components/AddPlans/TimePicker.js';
 import BasicCard from '../components/Card/BasicCard.js';
 
-import { Grid, TextField } from '@mui/material';
+import { Grid } from '@mui/material';
 import BasicTemplate from '../templates/BasicTemplate.js';
+import Inputs from '../components/AddPlans/Inputs.js';
+import Table from '../components/AddPlans/Table.js';
+import Container from '@mui/material/Container';
+import styles from './AddPlans.module.css';
 
 export default function AddPlans() {
   return (
     <BasicTemplate>
-      <Grid container spacing={20}>
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <BasicCard>
+      <Container maxWidth="lg" sx={{ mt: 5 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <BasicCard title="등록">
+              <Inputs />
               <TimePicker />
             </BasicCard>
           </Grid>
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <BasicCard>
-              <TimePicker />
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <BasicCard title="직원목록">
+              <Table />
             </BasicCard>
           </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </BasicTemplate>
   );
 }
