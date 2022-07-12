f9877e9c338ddf0017d76d3abfaf7f5da0545cac
Jun0zo <joon0zo1022@gmail.com>
2022-07-12 09:05:06 +0900

    attendance db query 수정

--- backend/routes/api/attendance.js
+++ backend/routes/api/attendance.js
@@ -1,16 +1,23 @@
-import express from 'express';
-import db from '../../helpers/db.js';
-const router = express.Router()
+import express from "express";
+import db from "../../helpers/db.js";
+const router = express.Router();
 
-router.post('/attendance', (req, res) => {
-  let date = req.body.start_date;
-  db.all(`SELECT * FROM attendance WHERE attendance_date >= ${start_date} AND attendance_date <= ${end_date}`, (err, rows) => {
-    if (err) {
-      return console.error(err.message)
+router.post("/attendance", (req, res) => {
+  let start_date = req.body.start_date;
+  let end_date = req.body.end_date;
+  db.all(
+    `SELECT * FROM attendance
+    INNER JOIN user ON attendance.attendance_id = user.id
+    INNER JOIN company ON attendance.company_id = company.company_id
+    WHERE attendance_date >= "${start_date}" AND attendance_date <= "${end_date}";`,
+    (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.json(rows);
     }
-    res.json(rows);
-  });
-})
+  );
+});
 
 module.exports = router;
 // export default router;
--- frontend/src/_actions/attendance.actions.js
+++ frontend/src/_actions/attendance.actions.js
@@ -1,7 +1,7 @@
 import { useRecoilState } from 'recoil';
 
 import { attendanceAtom } from '../_state/attendance.js';
-import { useFetchWrapper } from '../_helpers/fetch-wrapper.js'
+import { useFetchWrapper } from '../_helpers/fetch-wrapper.js';
 import { dateFormat } from '../_helpers/formatting.js';
 
 export function useActions() {
@@ -9,10 +9,19 @@ export function useActions() {
   const [attendance, setattendance] = useRecoilState(attendanceAtom);
 
   const get = (date) => {
-    let start_date = dateFormat(date, 'YYYY-MM') + '-00 00:00';
-    let end_date = dateFormat(date, 'YYYY-MM') + '-00 00:00';
-    return fetchWrapper.post('http://localhost:9000/api/attendance', {start_date, end_date}).then(setattendance)
-  }
+    let start_date = new Date(date);
+    let end_date = new Date(date);
+    end_date.setMonth(date.getMonth() + 1);
+    console.log(start_date, end_date);
+    let start_date_string = dateFormat(start_date, 'YYYY-MM') + '-00 00:00';
+    let end_date_string = dateFormat(end_date, 'YYYY-MM') + '-00 00:00';
+    return fetchWrapper
+      .post('http://localhost:9000/api/attendance', {
+        start_date: start_date_string,
+        end_date: end_date_string,
+      })
+      .then(setattendance);
+  };
 
-  return {get}
+  return { get };
 }
